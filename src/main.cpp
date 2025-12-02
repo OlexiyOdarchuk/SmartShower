6d288090a6148aea3354719bfa10819183ca71fd
iShawyha <shawyhaf@gmail.com>
2025-12-02 21:27:17 +0200

    feat(bot): fisrt try. add connect and first message

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,36 @@
 #include <FastBot2.h>
 #include <WiFi.h>
 
-void setup() {}
-void loop() {}
+FastBot2 bot(BOT_TOKEN);
+
+void updateh(fb::Update &u)
+{
+    Serial.println("New message!");
+
+    if (u.message().text() == "/start")
+    {
+        fb::Message msg;
+        msg.chatID = u.message().chat().id();
+        msg.text = "Hello";
+        bot.sendMessage(msg, false);
+    }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("Connected");
+    Serial.println(WiFi.localIP());
+    bot.setPollMode(fb::Poll::Long, 60000);
+}
+void loop()
+{
+    bot.attachUpdate(updateh);
+    bot.tick();
+}
