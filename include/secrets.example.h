3530fcc909faadfbdd0020ee60a5bcf9dea02dfe
iShawyha <shawyhaf@gmail.com>
2025-12-02 19:46:02 +0200

    init

---
+++ include/secrets.example.h
@@ -0,0 +1,4 @@
+#define WIFI_SSID "MyWifi"
+#define WIFI_PASS "mySuperPuperParol"
+#define BOT_TOKEN "0123456789:BOTShowERToNKen_CLI"
+#define ADMIN_ID "1433760480"
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+#include <secrets.h>
+#include <TM1637Display.h>
+#include <FastBot2.h>
+#include <WiFi.h>
+
+void setup() {}
+void loop() {}
